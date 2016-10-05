4a3a1b914fb09c6344f9c7c2b44522cf95458d7f
red <hey.red@outlook.com>
2016-10-06 02:33:08 +0300

    Now used Apache mime type -> extension map.

--- src/Mime/Mime.cs
+++ src/Mime/Mime.cs
@@ -72,60 +72,30 @@ namespace HeyRed.MimeGuesser
         /// Get file extension from path
         /// </summary>
         /// <param name="filePath"></param>
-        /// <returns>First extension as string or empty string if extension not found</returns>
+        /// <returns>Extension as string</returns>
         public static string GuessExtension(string filePath)
         {
-            if (filePath == null)
-            {
-                throw new ArgumentNullException("filePath");
-            }
-
-            using (var magic = new Magic(
-                MagicOpenFlags.MAGIC_ERROR |
-                MagicOpenFlags.MAGIC_EXTENSION))
-            {
-                return magic.Read(filePath).Split('/')[0];
-            }
+            return ApacheMimeTypes.LookupExtension(GuessMimeType(filePath));
         }
 
         /// <summary>
         /// Get file extension from bytes buffer
         /// </summary>
         /// <param name="buffer"></param>
-        /// <returns>First extension as string or empty string if extension not found</returns>
+        /// <returns>Extension as string</returns>
         public static string GuessExtension(byte[] buffer)
         {
-            if (buffer == null)
-            {
-                throw new ArgumentNullException("buffer");
-            }
-
-            using (var magic = new Magic(
-                MagicOpenFlags.MAGIC_ERROR |
-                MagicOpenFlags.MAGIC_EXTENSION))
-            {
-                return magic.Read(buffer).Split('/')[0];
-            }
+            return ApacheMimeTypes.LookupExtension(GuessMimeType(buffer));
         }
 
         /// <summary>
         /// Get file extension from stream
         /// </summary>
         /// <param name="stream"></param>
-        /// <returns>First extension as string or empty string if extension not found</returns>
+        /// <returns>Extension as string</returns>
         public static string GuessExtension(Stream stream)
         {
-            if (stream == null)
-            {
-                throw new ArgumentNullException("stream");
-            }
-
-            using (var magic = new Magic(
-                MagicOpenFlags.MAGIC_ERROR |
-                MagicOpenFlags.MAGIC_EXTENSION))
-            {
-                return magic.Read(stream).Split('/')[0];
-            }
+            return ApacheMimeTypes.LookupExtension(GuessMimeType(stream));
         }
     }
 }
