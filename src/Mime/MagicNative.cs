4e63dba84936f91674050ef61c96e867c9cac7f2
red <hey.red@outlook.com>
2016-10-29 23:49:42 +0300

    Small refactoring

--- src/Mime/MagicNative.cs
+++ src/Mime/MagicNative.cs
@@ -5,25 +5,27 @@ namespace HeyRed.MimeGuesser
 {
     internal static class MagicNative
     {
-        [DllImport("libmagic1", CallingConvention = CallingConvention.Cdecl)]
+        private const string MAGIC_LIB_PATH = "libmagic1";
+
+        [DllImport(MAGIC_LIB_PATH, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr magic_open(MagicOpenFlags flags);
 
-        [DllImport("libmagic1", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(MAGIC_LIB_PATH, CallingConvention = CallingConvention.Cdecl)]
         public static extern int magic_load(IntPtr magic_cookie, string filename);
 
-        [DllImport("libmagic1", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(MAGIC_LIB_PATH, CallingConvention = CallingConvention.Cdecl)]
         public static extern void magic_close(IntPtr magic_cookie);
 
-        [DllImport("libmagic1", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(MAGIC_LIB_PATH, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr magic_file(IntPtr magic_cookie, string filename);
 
-        [DllImport("libmagic1", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(MAGIC_LIB_PATH, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr magic_buffer(IntPtr magic_cookie, byte[] buffer, int length);
 
-        [DllImport("libmagic1", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(MAGIC_LIB_PATH, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr magic_error(IntPtr magic_cookie);
 
-        [DllImport("libmagic1", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(MAGIC_LIB_PATH, CallingConvention = CallingConvention.Cdecl)]
         public static extern int magic_version();
     }
 }
