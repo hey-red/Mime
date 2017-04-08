37ab3fa6656ab93170ce615e8f2b1f8b59c8ddaa
red <hey.red@outlook.com>
2017-04-08 14:15:42 +0300

    Added version test

---
+++ test/MimeTests/MagicMeta.cs
@@ -0,0 +1,11 @@
+﻿using HeyRed.Mime;
+using Xunit;
+
+namespace MimeTests
+{
+    public class MagicMeta
+    {
+        [Fact]
+        public void CheckVersion() => Assert.Equal(530, Magic.Version);
+    }
+}
