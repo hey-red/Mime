ad882939d9b97f4d466759a0e65b876a3504c25a
hey-red <hey.red@outlook.com>
2020-01-10 18:13:48 +0300

    Add description for magic params

--- src/Mime/MagicParams.cs
+++ src/Mime/MagicParams.cs
@@ -2,12 +2,33 @@
 {
     public enum MagicParams
     {
+        /// <summary>
+        /// The parameter controls how many levels of recursion will be followed for indirect magic entries.
+        /// </summary>
         MAGIC_PARAM_INDIR_MAX = 0,
+
+        /// <summary>
+        /// The parameter controls the maximum number of calls for name/use.
+        /// </summary>
         MAGIC_PARAM_NAME_MAX,
+
+        /// <summary>
+        /// The parameter controls how many ELF program sections will be processed.
+        /// </summary>
         MAGIC_PARAM_ELF_PHNUM_MAX,
+
+        /// <summary>
+        /// The parameter controls how many ELF sections will be processed.
+        /// </summary>
         MAGIC_PARAM_ELF_SHNUM_MAX,
+
+        /// <summary>
+        /// The parameter controls how many ELF notes will be processed.
+        /// </summary>
         MAGIC_PARAM_ELF_NOTES_MAX,
+
         MAGIC_PARAM_REGEX_MAX,
+
         MAGIC_PARAM_BYTES_MAX
     }
 }
