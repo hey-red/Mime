902f1eb23780a7d0a85f52ea9634cc0563af8800
red <vforviolence@outlook.com>
2022-05-22 07:39:29 +0300

    Search database in runtimes directory

--- src/Mime/MagicUtils.cs
+++ src/Mime/MagicUtils.cs
@@ -1,15 +1,44 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace HeyRed.Mime
 {
     internal static class MagicUtils
     {
+        private const string MAGIC_DB_NAME = "magic.mgc";
+
+        private static string GetCurrentPlatform()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return "win";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return "linux";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                return "osx";
+            throw new PlatformNotSupportedException();
+        }
+
         public static string GetDefaultMagicPath()
         {
             string assemblyLocation = typeof(MagicUtils).GetTypeInfo().Assembly.Location;
-            string magicDbPath = Path.Combine(assemblyLocation.Replace("Mime.dll", ""), "magic.mgc");
+            string currentPath = assemblyLocation.Replace("Mime.dll", "");
 
+            string magicDbPath = Path.Combine(currentPath, MAGIC_DB_NAME);
+
+            // Find inside current directory
+            if (File.Exists(magicDbPath))
+            {
+                return magicDbPath;
+            }
+
+            var rid = GetCurrentPlatform();
+            var architecture = RuntimeInformation.ProcessArchitecture.ToString().ToLower();
+
+            magicDbPath = Path.Combine(currentPath, $"runtimes/{rid}-{architecture}/native/", MAGIC_DB_NAME);
+
+            // Find inside runtimes directory
             if (File.Exists(magicDbPath))
             {
                 return magicDbPath;
